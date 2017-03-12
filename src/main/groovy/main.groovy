69739759f2612d14912d6a5d8ed0d912ae3dd624
Evan O'Connell <oconn.e@gmail.com>
2017-03-11 22:21:23 -0500

    Handle nested objects

--- src/main/groovy/main.groovy
+++ src/main/groovy/main.groovy
@@ -5,6 +5,11 @@ class Foo {
   String bar, baz, qux
 }
 
+class Bar {
+  String dog
+  Foo foo
+}
+
 class DataBag implements GroovyInterceptable {
 
   PdxInstance pdx
@@ -14,22 +19,34 @@ class DataBag implements GroovyInterceptable {
   }
 
   def getProperty(String field) {
-    pdx.getField(field)
+    def o = pdx.getField(field)
+    (o in PdxInstance) ? new DataBag(pdx: o) : o
   }
 }
 
 println "Hello world!"
 
 def cache = new CacheFactory().create()
+cache.setReadSerialized(true)
 
-PdxInstance instance = cache.createPdxInstanceFactory("Foo")
+PdxInstance instanceOfFoo = cache.createPdxInstanceFactory("Foo")
   .writeString("bar", "1")
   .writeString("baz", "2")
   .writeString("qux", "3")
   .create()
 
-def o = new DataBag(pdx: instance)
+PdxInstance instance = cache.createPdxInstanceFactory("Bar")
+  .writeString("dog", "bark")
+  .writeObject("foo", instanceOfFoo)
+  .create()
+
+def o = new DataBag(pdx: instanceOfFoo)
 
 println o.bar // prints 1
 println o.baz // prints 2
 println o.qux // prints 3
+println o.nah // prints null
+
+def x = new DataBag(pdx: instance)
+println x.dog
+println x.foo.bar // prints 1
